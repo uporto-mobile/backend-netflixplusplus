f7e8f29b6cb96273cdd0c487cad2e97979c2d45a
Lucas Ribeiro <lucas.ribeiro@smarttbotter.com>
2022-10-18 21:14:50 +0100

    feat: project setup

---
+++ src/main/java/com/lmr/backendnetflixplusplus/BackendNetflixplusplusApplication.java
@@ -0,0 +1,13 @@
+package com.lmr.backendnetflixplusplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNetflixplusplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNetflixplusplusApplication.class, args);
+	}
+
+}
